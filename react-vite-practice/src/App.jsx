9eeee96b61024e1757d96b8fbae3527060ce827d
kingbarz <mrnwobodo@gmail.com>
2022-10-19 02:17:01 +0100

    first commit

---
+++ react-vite-practice/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header"
+import MainComponent from "./components/MainComponent"
+import "./App.css"
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <MainComponent />
+
+    </div>
+  )
+}
+
+export default App
